a17f6c9c82e2f3de9b5cbff6f80f2e60cd6f79dc
ALI-x1 <alimbaw82@gmail.com>
2026-08-07 21:30:09 +0330

    Add androidx.core:core-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ dependencies {
   implementation(libs.moshi.kotlin)
   implementation(libs.okhttp)
   implementation(libs.retrofit)
+  implementation("androidx.core:core-ktx:1.13.1")
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
