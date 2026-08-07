6d7a814f59cd2fc18d7aaf32580f910d57fd0377
ALI-x1 <alimbaw82@gmail.com>
2026-08-07 20:57:37 +0330

    Remove appcompat dependency
    Removed appcompat dependency from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,6 @@ dependencies {
   implementation(libs.moshi.kotlin)
   implementation(libs.okhttp)
   implementation(libs.retrofit)
-  implementation("androidx.appcompat:appcompat:1.7.0")
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
